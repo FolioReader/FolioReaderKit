0287557496f4f1c771ded11a0105927be75259aa
Heberti Almeida <hebertialmeida@gmail.com>
2015-04-09 17:31:01 -0300

    Testing WKWebViews

---
+++ FolioReaderKit/FolioReaderBook.swift
@@ -0,0 +1,13 @@
+//
+//  FolioReaderBook.swift
+//  FolioReaderKit
+//
+//  Created by Heberti Almeida on 09/04/15.
+//  Copyright (c) 2015 Folio Reader. All rights reserved.
+//
+
+import UIKit
+
+class FolioReaderBook: NSObject {
+   
+}
---
+++ FolioReaderKit/FolioReaderIndex.swift
@@ -0,0 +1,13 @@
+//
+//  FolioReaderIndex.swift
+//  FolioReaderKit
+//
+//  Created by Heberti Almeida on 09/04/15.
+//  Copyright (c) 2015 Folio Reader. All rights reserved.
+//
+
+import UIKit
+
+class FolioReaderIndex: NSObject {
+   
+}
