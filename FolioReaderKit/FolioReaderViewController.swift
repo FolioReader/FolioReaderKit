6290624382148fa46be44e0ee54255499572a654
Heberti Almeida <hebertialmeida@gmail.com>
2015-04-15 11:16:25 -0300

    Improve the current page calc method

--- FolioReaderKit/FolioReaderViewController.swift
+++ FolioReaderKit/FolioReaderViewController.swift
@@ -32,9 +32,9 @@ class FolioReaderViewController: UIViewController, UICollectionViewDelegate, UIC
     var collectionView: UICollectionView!
     var pages: [String]!
     var totalPages: Int!
-    var currentPageNumber: Int!
     var pageWidth: CGFloat!
     var pageHeight: CGFloat!
+    var currentPageNumber: Int!
     var currentPage: FolioReaderPage!
     var delegate: FolioReaderViewControllerDelegate!
     
@@ -67,6 +67,7 @@ class FolioReaderViewController: UIViewController, UICollectionViewDelegate, UIC
         collectionView.pagingEnabled = true
         collectionView.showsVerticalScrollIndicator = false
         collectionView.backgroundColor = UIColor.whiteColor()
+        collectionView.decelerationRate = UIScrollViewDecelerationRateFast
         self.view.addSubview(collectionView)
         
         // Register cell classes
@@ -75,7 +76,7 @@ class FolioReaderViewController: UIViewController, UICollectionViewDelegate, UIC
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        setCurrentPageNumber()
+        setCurrentPage()
 //        delegate.readerDidAppear()
     }
 
@@ -101,7 +102,7 @@ class FolioReaderViewController: UIViewController, UICollectionViewDelegate, UIC
         cell.delegate = self
         
         // Configure the cell
-        let demoStr = "<?xml version=\"1.0\" encoding=\"utf-8\"?> <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"> <head> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\"> <meta name=\"generator\" content= \"HTML Tidy for FreeBSD (vers 7 December 2008), see www.w3.org\" /> <title>Romeo and Juliet</title> <style>html {-webkit-text-size-adjust: none;} * { padding: 0; margin: 0; outline: none; list-style: none; border: 0 none; } /* Typography & Colors */ body { font: 19px/1.6em helvetica, sans-serif; padding: 40px 30px; color: #383737; } .content-title { font: 35px/1.3em helvetica, sans-serif; margin-bottom: 35px; color: black; } .author { font: 20px/1.5em helvetica, sans-serif; margin-bottom: 25px; color: lightgray; } b, strong { font-family: helvetica, sans-serif; } i, em { font-family: helvetica, sans-serif; } a { color: #004270; } .wp-caption p.wp-caption-text { color: #888; margin: 0 5px; font-size: 14px; } .gallery-caption { font-size: 14px; line-height: 1.3; } /* Layout */ .alignleft { float: left; } img.alignleft { padding: 5px; margin-right: 20px; display: inline; background: #f1f1f1; } p { padding-top: 0.8em; padding-bottom: 0.6em; } .gallery-icon { margin: 10px 0; } .wp-caption.alignleft { margin: 1em 20px 0 0; } .wp-caption { background: #f1f1f1; line-height: 18px; margin-bottom: 20px; max-width: 100% !important; padding-top: 5px; text-align: center; }</style> <meta http-equiv=\"Content-Type\" content= \"application/xhtml+xml; charset=utf-8\" /> </head> <body> <div class=\"body\"> <div id=\"chapter_3521\" class=\"chapter\"> <h2><span class=\"chapterHeader\"><span class= \"translation\">Chapter</span> <span class= \"count\">\(indexPath.row+1)</span></span></h2> <p>&#160;</p> <div class=\"text\"> <p>In the evening Andrew and Pierre got into the open carriage and drove to Bald Hills. Prince Andrew, glancing at Pierre, broke the silence now and then with remarks which showed that he was in a good temper.</p> <p>Pointing to the fields, he spoke of the improvements he was making in his husbandry.</p> <p>Pierre remained gloomily silent, answering in monosyllables and apparently immersed in his own thoughts.</p> <p>He was thinking that Prince Andrew was unhappy, had gone astray, did not see the true light, and that he, Pierre, ought to aid, enlighten, and raise him. But as soon as he thought of what he should say, he felt that Prince Andrew with one word, one argument, would upset all his teaching, and he shrank from beginning, afraid of exposing to possible ridicule what to him was precious and sacred.</p> <p>\"No, but why do you think so?\" Pierre suddenly began, lowering his head and looking like a bull about to charge, \"why do you think so? You should not think so.\"</p> </div> </div> </div> </body> </html>"
+        let demoStr = "<?xml version=\"1.0\" encoding=\"utf-8\"?> <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"> <head> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\"> <meta name=\"generator\" content= \"HTML Tidy for FreeBSD (vers 7 December 2008), see www.w3.org\" /> <title>Romeo and Juliet</title> <style>html {-webkit-text-size-adjust: none;} * { padding: 0; margin: 0; outline: none; list-style: none; border: 0 none; } /* Typography & Colors */ body { font: 19px/1.6em helvetica, sans-serif; padding: 40px 30px; color: #383737; } .content-title { font: 35px/1.3em helvetica, sans-serif; margin-bottom: 35px; color: black; } .author { font: 20px/1.5em helvetica, sans-serif; margin-bottom: 25px; color: lightgray; } b, strong { font-family: helvetica, sans-serif; } i, em { font-family: helvetica, sans-serif; } a { color: #004270; } .wp-caption p.wp-caption-text { color: #888; margin: 0 5px; font-size: 14px; } .gallery-caption { font-size: 14px; line-height: 1.3; } /* Layout */ .alignleft { float: left; } img.alignleft { padding: 5px; margin-right: 20px; display: inline; background: #f1f1f1; } p { padding-top: 0.8em; padding-bottom: 0.6em; } .gallery-icon { margin: 10px 0; } .wp-caption.alignleft { margin: 1em 20px 0 0; } .wp-caption { background: #f1f1f1; line-height: 18px; margin-bottom: 20px; max-width: 100% !important; padding-top: 5px; text-align: center; }</style> <meta http-equiv=\"Content-Type\" content= \"application/xhtml+xml; charset=utf-8\" /> </head> <body> <div class=\"body\"> <div id=\"chapter_3521\" class=\"chapter\"> <h2><span class=\"chapterHeader\"><span class= \"translation\">Chapter</span> <span class= \"count\">\(indexPath.row+1)</span></span></h2> <p>&#160;</p> <div class=\"text\"> <p>In the evening Andrew and Pierre got into the open carriage and drove to Bald Hills. Prince Andrew, glancing at Pierre, broke the silence now and then with remarks which showed that he was in a good temper.</p> <p>Pointing to the fields, he spoke of the improvements he was making in his husbandry.</p> <p>Pierre remained gloomily silent, answering in monosyllables and apparently immersed in his own thoughts.</p> <p>He was thinking that Prince Andrew was unhappy, had gone astray, did not see the true light, and that he, Pierre, ought to aid, enlighten, and raise him. But as soon as he thought of what he should say, he felt that Prince Andrew with one word, one argument, would upset all his teaching, and he shrank from beginning, afraid of exposing to possible ridicule what to him was precious and sacred.</p> <p>\"No, but why do you think so?\" Pierre suddenly began, lowering his head and looking like a bull about to charge, \"why do you think so? You should not think so.\"</p> <p>In the evening Andrew and Pierre got into the open carriage and drove to Bald Hills. Prince Andrew, glancing at Pierre, broke the silence now and then with remarks which showed that he was in a good temper.</p> <p>Pointing to the fields, he spoke of the improvements he was making in his husbandry.</p> <p>Pierre remained gloomily silent, answering in monosyllables and apparently immersed in his own thoughts.</p> <p>He was thinking that Prince Andrew was unhappy, had gone astray, did not see the true light, and that he, Pierre, ought to aid, enlighten, and raise him. But as soon as he thought of what he should say, he felt that Prince Andrew with one word, one argument, would upset all his teaching, and he shrank from beginning, afraid of exposing to possible ridicule what to him was precious and sacred.</p> <p>\"No, but why do you think so?\" Pierre suddenly began, lowering his head and looking like a bull about to charge, \"why do you think so? You should not think so.\"</p> <p>In the evening Andrew and Pierre got into the open carriage and drove to Bald Hills. Prince Andrew, glancing at Pierre, broke the silence now and then with remarks which showed that he was in a good temper.</p> <p>Pointing to the fields, he spoke of the improvements he was making in his husbandry.</p> <p>Pierre remained gloomily silent, answering in monosyllables and apparently immersed in his own thoughts.</p> <p>He was thinking that Prince Andrew was unhappy, had gone astray, did not see the true light, and that he, Pierre, ought to aid, enlighten, and raise him. But as soon as he thought of what he should say, he felt that Prince Andrew with one word, one argument, would upset all his teaching, and he shrank from beginning, afraid of exposing to possible ridicule what to him was precious and sacred.</p> <p>\"No, but why do you think so?\" Pierre suddenly began, lowering his head and looking like a bull about to charge, \"why do you think so? You should not think so.\"</p> </div> </div> </div> </body> </html>"
         
         cell.loadHTMLString(demoStr)
         
@@ -122,7 +123,7 @@ class FolioReaderViewController: UIViewController, UICollectionViewDelegate, UIC
     
     override func willRotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
         setPageSize(toInterfaceOrientation)
-        setCurrentPageNumber()
+        setCurrentPage()
         
         UIView.animateWithDuration(duration, animations: { () -> Void in
             self.collectionView.contentSize = CGSizeMake(self.pageWidth, self.pageHeight * CGFloat(self.totalPages))
@@ -146,9 +147,39 @@ class FolioReaderViewController: UIViewController, UICollectionViewDelegate, UIC
         pageHeight = orientation.isPortrait ? screenBounds.size.height : screenBounds.size.width
     }
     
-    func setCurrentPageNumber() {
-        let indexPath = self.collectionView.indexPathsForVisibleItems().first as! NSIndexPath
-        currentPageNumber = indexPath.row+1
+    func setCurrentPage() {
+        let currentIndexPath = getCurrentIndexPath()
+        currentPage = collectionView.cellForItemAtIndexPath(currentIndexPath) as! FolioReaderPage
+        currentPageNumber = currentIndexPath.row+1
+    }
+    
+    func getCurrentIndexPath() -> NSIndexPath {
+        let indexPaths = self.collectionView.indexPathsForVisibleItems()
+        var indexPath = NSIndexPath()
+        
+        if indexPaths.count > 1 {
+            let first = indexPaths.first as! NSIndexPath
+            let last = indexPaths.last as! NSIndexPath
+            
+            switch scrollDirection {
+            case .Up:
+                if first.compare(last) == NSComparisonResult.OrderedAscending {
+                    indexPath = last
+                } else {
+                    indexPath = first
+                }
+            default:
+                if first.compare(last) == NSComparisonResult.OrderedAscending {
+                    indexPath = first
+                } else {
+                    indexPath = last
+                }
+            }
+        } else {
+            indexPath = indexPaths.first as! NSIndexPath
+        }
+        
+        return indexPath
     }
     
     func frameForPage(page: Int) -> CGRect {
@@ -163,14 +194,21 @@ class FolioReaderViewController: UIViewController, UICollectionViewDelegate, UIC
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
         scrollDirection = scrollView.contentOffset.y < pointNow.y ? .Down : .Up
-
+        
         if scrollView is UICollectionView {
-            
+
         } else {
-            let contentHeight = scrollView.contentSize.height - scrollView.bounds.height
-            if scrollView.contentOffset.y >= contentHeight {
-                return
-            }
+//            let contentHeight = scrollView.contentSize.height - scrollView.bounds.height
+//            if scrollView.contentOffset.y >= contentHeight {
+//                return
+//            }
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        if scrollView is UICollectionView {
+            setCurrentPage()
+            println("page: \(currentPageNumber)")
         }
     }
     
