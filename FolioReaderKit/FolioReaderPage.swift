28444fbefb05a62ff7d2eb83164b989922a85658
Heberti Almeida <hebertialmeida@gmail.com>
2015-04-17 09:32:02 -0300

    Removed WKWebView, now using just UIWebView

--- FolioReaderKit/FolioReaderPage.swift
+++ FolioReaderKit/FolioReaderPage.swift
@@ -7,48 +7,30 @@
 //
 
 import UIKit
-import WebKit
 
 @objc protocol FolioPageDelegate {
     optional func pageDidLoad(page: FolioReaderPage)
 }
 
-//private let hasWebKit = NSClassFromString("WKWebView") != nil
-private let hasWebKit = false
-
-class FolioReaderPage: UICollectionViewCell, WKNavigationDelegate, UIWebViewDelegate {
+class FolioReaderPage: UICollectionViewCell, UIWebViewDelegate {
     
-    var webView: AnyObject!
+    var webView: UIWebView!
     var delegate: FolioPageDelegate!
+    
+    // MARK: - View life cicle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         self.backgroundColor = UIColor.whiteColor()
         
         if webView == nil {
-            if hasWebKit {
-                let config = WKWebViewConfiguration()
-                webView = WKWebView(frame: self.bounds, configuration: config)
-            } else {
-                webView = UIWebView(frame: self.bounds)
-            }
+            webView = UIWebView(frame: self.bounds)
         }
         
-        if hasWebKit {
-            let wkWebView = (webView as! WKWebView)
-            wkWebView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
-            wkWebView.navigationDelegate = self
-            wkWebView.backgroundColor = UIColor.whiteColor()
-            self.addSubview(wkWebView)
-        }
-        else {
-            let uiWebView = (webView as! UIWebView)
-            uiWebView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
-            uiWebView.delegate = self
-            uiWebView.backgroundColor = UIColor.whiteColor()
-            self.addSubview(uiWebView)
-        }
+        webView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        webView.delegate = self
+        webView.backgroundColor = UIColor.whiteColor()
+        self.addSubview(webView)
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -56,34 +38,8 @@ class FolioReaderPage: UICollectionViewCell, WKNavigationDelegate, UIWebViewDele
     }
     
     func loadHTMLString(string: String) {
-        if hasWebKit {
-            var castWebView = (webView as! WKWebView)
-            castWebView.alpha = 0
-            castWebView.loadHTMLString(string, baseURL: nil)
-        } else {
-            var castWebView = (webView as! UIWebView)
-            castWebView.alpha = 0
-            castWebView.loadHTMLString(string, baseURL: nil)
-        }
-    }
-    
-    // MARK: - WKWebView Navigation Delegate
-    
-    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
-        if scrollDirection == .Down {
-            let bottomOffset = CGPointMake(0, webView.scrollView.contentSize.height - webView.scrollView.bounds.height)
-            if bottomOffset.y >= 0 {
-                dispatch_async(dispatch_get_main_queue(), {
-                    webView.scrollView.setContentOffset(bottomOffset, animated: false)
-                })
-            }
-        }
-        
-        UIView.animateWithDuration(0.2, animations: { () -> Void in
-            webView.alpha = 1
-        })
-        
-        delegate.pageDidLoad!(self)
+        webView.alpha = 0
+        webView.loadHTMLString(string, baseURL: nil)
     }
     
     // MARK: - UIWebView Delegate
@@ -104,5 +60,4 @@ class FolioReaderPage: UICollectionViewCell, WKNavigationDelegate, UIWebViewDele
         
         delegate.pageDidLoad!(self)
     }
-    
 }
