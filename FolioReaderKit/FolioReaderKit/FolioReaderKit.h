c974588a853bceb343f2b23fef8c97d94ea52853
alexpopov <hello@alexpopov.ca>
2016-06-04 12:13:24 -0700

    created empty FolioReaderKit project

---
+++ FolioReaderKit/FolioReaderKit/FolioReaderKit.h
@@ -0,0 +1,19 @@
+//
+//  FolioReaderKit.h
+//  FolioReaderKit
+//
+//  Created by Alex Popov on 2016-06-04.
+//  Copyright © 2016 FolioReader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FolioReaderKit.
+FOUNDATION_EXPORT double FolioReaderKitVersionNumber;
+
+//! Project version string for FolioReaderKit.
+FOUNDATION_EXPORT const unsigned char FolioReaderKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FolioReaderKit/PublicHeader.h>
+
+
