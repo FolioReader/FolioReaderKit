6e26682e1ae06aab31cbcd3f26e45c55448ac2ba
Heberti Almeida <hebertialmeida@gmail.com>
2016-08-09 00:13:27 -0300

    Fixed page count bug

--- Source/FolioReaderCenter.swift
+++ Source/FolioReaderCenter.swift
@@ -1003,12 +1003,8 @@ extension FolioReaderCenter: FolioReaderPageDelegate {
                 if currentPageNumber == pageNumber && pageOffset > 0 {
                     page.scrollPageToOffset(pageOffset, animated: false)
                 }
-            } else {
-                updateCurrentPage(page)
-                
-                if !isScrolling && FolioReader.needsRTLChange {
-                    page.scrollPageToBottom()
-                }
+            } else if !isScrolling && FolioReader.needsRTLChange {
+                page.scrollPageToBottom()
             }
         } else if isFirstLoad {
             updateCurrentPage(page)
