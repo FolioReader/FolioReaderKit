263668174f31229025e9b99e00ddac622ffc0f7e
Paulo Coutinho <paulo@prsolucoes.com>
2017-12-27 16:02:18 -0200

    swift4

--- Source/FolioReaderCenter.swift
+++ Source/FolioReaderCenter.swift
@@ -920,10 +920,7 @@ open class FolioReaderCenter: UIViewController, UICollectionViewDelegate, UIColl
         }
         
         UIView.animate(withDuration: animated ? 0.3 : 0, delay: 0, options: UIViewAnimationOptions(), animations: { () -> Void in
-            cell.scrollPageToOffset(contentOffsetX, animated: animated)
-            self.updateCurrentPage {
-                completion?()
-            }
+            cell.scrollPageToOffset(contentOffsetX, animated: animated)            
         }) { (finished: Bool) -> Void in
             self.updateCurrentPage {
                 completion?()
