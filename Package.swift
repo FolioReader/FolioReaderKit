2036fbfbc8cf5ef9f0a716224e56765d7f9d43c4
Heberti Almeida <hebertialmeida@gmail.com>
2018-02-12 14:36:43 -0500

    Update dependencies versions

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
 		.Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 2),
 		.Package(url: "https://github.com/ArtSabintsev/FontBlaster.git", majorVersion: 4, minor: 0),
 		.Package(url: "https://github.com/jessesquires/JSQWebViewController.git", majorVersion: 6, minor: 0),
-		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 0),
+		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 1),
 	]
 )
