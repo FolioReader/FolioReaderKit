92f95da0beeec9a4ad4d70b6535f21aecf2122ab
fantim <timfan@berkeley.edu>
2019-01-12 16:49:44 +0800

    Fixed a thread lock issue causing example to Example to crash

--- Source/Extensions.swift
+++ Source/Extensions.swift
@@ -31,7 +31,7 @@ extension CGSize {
     func forDirection(withConfiguration readerConfig: FolioReaderConfig) -> CGFloat {
         return readerConfig.isDirection(height, width, height)
     }
-
+    
     func forReverseDirection(withConfiguration readerConfig: FolioReaderConfig) -> CGFloat {
         return readerConfig.isDirection(width, height, width)
     }
@@ -47,7 +47,7 @@ extension ScrollDirection {
     static func negative(withConfiguration readerConfig: FolioReaderConfig, scrollType: ScrollType = .page) -> ScrollDirection {
         return readerConfig.isDirection(.down, .right, .right)
     }
-
+    
     static func positive(withConfiguration readerConfig: FolioReaderConfig, scrollType: ScrollType = .page) -> ScrollDirection {
         return readerConfig.isDirection(.up, .left, .left)
     }
@@ -58,7 +58,7 @@ extension ScrollDirection {
 /**
  Delay function
  From: http://stackoverflow.com/a/24318861/517707
-
+ 
  - parameter delay:   Delay in seconds
  - parameter closure: Closure
  */
@@ -81,7 +81,7 @@ internal extension UIColor {
         var green: CGFloat = 0.0
         var blue:  CGFloat = 0.0
         var alpha: CGFloat = 1.0
-
+        
         if rgba.hasPrefix("#") {
             let index = rgba.index(rgba.startIndex, offsetBy: 1)
             let hex = String(rgba[index...])
@@ -123,7 +123,7 @@ internal extension UIColor {
         }
         self.init(red:red, green:green, blue:blue, alpha:alpha)
     }
-
+    
     //
     /// Hex string of a UIColor instance.
     ///
@@ -137,55 +137,55 @@ internal extension UIColor {
         var b: CGFloat = 0
         var a: CGFloat = 0
         self.getRed(&r, green: &g, blue: &b, alpha: &a)
-
+        
         if (includeAlpha == true) {
             return String(format: "#%02X%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255), Int(a * 255))
         } else {
             return String(format: "#%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
         }
     }
-
+    
     // MARK: - color shades
     // https://gist.github.com/mbigatti/c6be210a6bbc0ff25972
-
+    
     func highlightColor() -> UIColor {
-
+        
         var hue : CGFloat = 0
         var saturation : CGFloat = 0
         var brightness : CGFloat = 0
         var alpha : CGFloat = 0
-
+        
         if getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
             return UIColor(hue: hue, saturation: 0.30, brightness: 1, alpha: alpha)
         } else {
             return self;
         }
-
+        
     }
-
+    
     /**
      Returns a lighter color by the provided percentage
-
+     
      :param: lighting percent percentage
      :returns: lighter UIColor
      */
     func lighterColor(_ percent : Double) -> UIColor {
         return colorWithBrightnessFactor(CGFloat(1 + percent));
     }
-
+    
     /**
      Returns a darker color by the provided percentage
-
+     
      :param: darking percent percentage
      :returns: darker UIColor
      */
     func darkerColor(_ percent : Double) -> UIColor {
         return colorWithBrightnessFactor(CGFloat(1 - percent));
     }
-
+    
     /**
      Return a modified color using the brightness factor provided
-
+     
      :param: factor brightness factor
      :returns: modified color
      */
@@ -194,7 +194,7 @@ internal extension UIColor {
         var saturation : CGFloat = 0
         var brightness : CGFloat = 0
         var alpha : CGFloat = 0
-
+        
         if getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
             return UIColor(hue: hue, saturation: saturation, brightness: brightness * factor, alpha: alpha)
         } else {
@@ -214,32 +214,32 @@ internal extension String {
             return self
         }
     }
-
+    
     func stripHtml() -> String {
         return self.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression)
     }
-
+    
     func stripLineBreaks() -> String {
         return self.replacingOccurrences(of: "\n", with: "", options: .regularExpression)
     }
-
+    
     /**
      Converts a clock time such as `0:05:01.2` to seconds (`Double`)
-
+     
      Looks for media overlay clock formats as specified [here][1]
-
+     
      - Note: this may not be the  most efficient way of doing this. It can be improved later on.
-
+     
      - Returns: seconds as `Double`
-
+     
      [1]: http://www.idpf.org/epub/301/spec/epub-mediaoverlays.html#app-clock-examples
      */
     func clockTimeToSeconds() -> Double {
-
+        
         let val = self.trimmingCharacters(in: CharacterSet.whitespaces)
-
+        
         if( val.isEmpty ){ return 0 }
-
+        
         let formats = [
             "HH:mm:ss.SSS"  : "^\\d{1,2}:\\d{2}:\\d{2}\\.\\d{1,3}$",
             "HH:mm:ss"      : "^\\d{1,2}:\\d{2}:\\d{2}$",
@@ -247,98 +247,98 @@ internal extension String {
             "mm:ss"         : "^\\d{1,2}:\\d{2}$",
             "ss.SSS"         : "^\\d{1,2}\\.\\d{1,3}$",
             ]
-
+        
         // search for normal duration formats such as `00:05:01.2`
         for (format, pattern) in formats {
-
+            
             if val.range(of: pattern, options: .regularExpression) != nil {
-
+                
                 let formatter = DateFormatter()
                 formatter.dateFormat = format
                 let time = formatter.date(from: val)
-
+                
                 if( time == nil ){ return 0 }
-
+                
                 formatter.dateFormat = "ss.SSS"
                 let seconds = (formatter.string(from: time!) as NSString).doubleValue
-
+                
                 formatter.dateFormat = "mm"
                 let minutes = (formatter.string(from: time!) as NSString).doubleValue
-
+                
                 formatter.dateFormat = "HH"
                 let hours = (formatter.string(from: time!) as NSString).doubleValue
-
+                
                 return seconds + (minutes*60) + (hours*60*60)
             }
         }
-
+        
         // if none of the more common formats match, check for other possible formats
-
+        
         // 2345ms
         if val.range(of: "^\\d+ms$", options: .regularExpression) != nil{
             return (val as NSString).doubleValue / 1000.0
         }
-
+        
         // 7.25h
         if val.range(of: "^\\d+(\\.\\d+)?h$", options: .regularExpression) != nil {
             return (val as NSString).doubleValue * 60 * 60
         }
-
+        
         // 13min
         if val.range(of: "^\\d+(\\.\\d+)?min$", options: .regularExpression) != nil {
             return (val as NSString).doubleValue * 60
         }
-
+        
         return 0
     }
-
+    
     func clockTimeToMinutesString() -> String {
-
+        
         let val = clockTimeToSeconds()
-
+        
         let min = floor(val / 60)
         let sec = floor(val.truncatingRemainder(dividingBy: 60))
-
+        
         return String(format: "%02.f:%02.f", min, sec)
     }
-
+    
     // MARK: - NSString helpers
-
+    
     var lastPathComponent: String {
         return (self as NSString).lastPathComponent
     }
-
+    
     var deletingLastPathComponent: String {
         return (self as NSString).deletingLastPathComponent
     }
-
+    
     var deletingPathExtension: String {
         return (self as NSString).deletingPathExtension
     }
-
+    
     var pathExtension: String {
         return (self as NSString).pathExtension
     }
-
+    
     var abbreviatingWithTildeInPath: String {
         return (self as NSString).abbreviatingWithTildeInPath
     }
-
+    
     func appendingPathComponent(_ str: String) -> String {
         return (self as NSString).appendingPathComponent(str)
     }
-
+    
     func appendingPathExtension(_ str: String) -> String {
         return (self as NSString).appendingPathExtension(str) ?? self+"."+str
     }
 }
 
 internal extension UIImage {
-
+    
     convenience init?(readerImageNamed: String) {
         self.init(named: readerImageNamed, in: Bundle.frameworkBundle(), compatibleWith: nil)
     }
-
+    
     /// Forces the image to be colored with Reader Config tintColor
     ///
     /// - Parameter readerConfig: Current folio reader configuration.
@@ -346,38 +346,38 @@ internal extension UIImage {
     func ignoreSystemTint(withConfiguration readerConfig: FolioReaderConfig) -> UIImage? {
         return self.imageTintColor(readerConfig.tintColor)?.withRenderingMode(.alwaysOriginal)
     }
-
+    
     /**
      Colorize the image with a color
-
+     
      - parameter tintColor: The input color
      - returns: Returns a colored image
      */
     func imageTintColor(_ tintColor: UIColor) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
-
+        
         let context = UIGraphicsGetCurrentContext()
         context?.translateBy(x: 0, y: self.size.height)
         context?.scaleBy(x: 1.0, y: -1.0)
         context?.setBlendMode(CGBlendMode.normal)
-
+        
         let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height) as CGRect
         if let cgImage = self.cgImage {
             context?.clip(to: rect, mask:  cgImage)
         }
-
+        
         tintColor.setFill()
         context?.fill(rect)
-
+        
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-
+        
         return newImage
     }
-
+    
     /**
      Generate a image with a color
-
+     
      - parameter color: The input color
      - returns: Returns a colored image
      */
@@ -385,23 +385,23 @@ internal extension UIImage {
         let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
         UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
         let context = UIGraphicsGetCurrentContext()
-
+        
         if let color = color {
             color.setFill()
         } else {
             UIColor.white.setFill()
         }
-
+        
         context!.fill(rect)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-
+        
         return image!
     }
-
+    
     /**
      Generates a image with a `CALayer`
-
+     
      - parameter layer: The input `CALayer`
      - returns: Return a rendered image
      */
@@ -412,10 +412,10 @@ internal extension UIImage {
         UIGraphicsEndImageContext()
         return img!
     }
-
+    
     /**
      Generates a image from a `UIView`
-
+     
      - parameter view: The input `UIView`
      - returns: Return a rendered image
      */
@@ -429,16 +429,16 @@ internal extension UIImage {
 }
 
 internal extension UIViewController {
-
+    
     func setCloseButton(withConfiguration readerConfig: FolioReaderConfig) {
         let closeImage = UIImage(readerImageNamed: "icon-navbar-close")?.ignoreSystemTint(withConfiguration: readerConfig)
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: closeImage, style: .plain, target: self, action: #selector(dismiss as () -> Void))
     }
-
+    
     @objc func dismiss() {
         self.dismiss(nil)
     }
-
+    
     func dismiss(_ completion: (() -> Void)?) {
         DispatchQueue.main.async {
             self.dismiss(animated: true, completion: {
@@ -446,65 +446,56 @@ internal extension UIViewController {
             })
         }
     }
-
+    
     // MARK: - NavigationBar
-
+    
     func setTransparentNavigation() {
         let navBar = self.navigationController?.navigationBar
         navBar?.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-        navBar?.hideBottomHairline()
+        navBar?.isHidden = true
         navBar?.isTranslucent = true
     }
-
+    
     func setTranslucentNavigation(_ translucent: Bool = true, color: UIColor, tintColor: UIColor = UIColor.white, titleColor: UIColor = UIColor.black, andFont font: UIFont = UIFont.systemFont(ofSize: 17)) {
         let navBar = self.navigationController?.navigationBar
         navBar?.setBackgroundImage(UIImage.imageWithColor(color), for: UIBarMetrics.default)
-        navBar?.showBottomHairline()
+        navBar?.isHidden = false
         navBar?.isTranslucent = translucent
         navBar?.tintColor = tintColor
         navBar?.titleTextAttributes = [NSAttributedStringKey.foregroundColor: titleColor, NSAttributedStringKey.font: font]
     }
 }
+/**
+ Fix for Swift 4 / iOS 12
+ https://stackoverflow.com/questions/34452920/removing-the-hairline-under-navigation-bar
+ */
+private var navigationBarHairLine: UIImageView?
 
-internal extension UINavigationBar {
-
-    func hideBottomHairline() {
-        let navigationBarImageView = hairlineImageViewInNavigationBar(self)
-        navigationBarImageView!.isHidden = true
+func findHairlineImageViewUnderView(view: UIView?) -> UIImageView? {
+    guard let view = view else { return nil }
+    if view.isKind(of: UIImageView.classForCoder()) && view.bounds.height <= 1 {
+        return view as? UIImageView
     }
-
-    func showBottomHairline() {
-        let navigationBarImageView = hairlineImageViewInNavigationBar(self)
-        navigationBarImageView!.isHidden = false
-    }
-
-    fileprivate func hairlineImageViewInNavigationBar(_ view: UIView) -> UIImageView? {
-        if view.isKind(of: UIImageView.self) && view.bounds.height <= 1.0 {
-            return (view as! UIImageView)
+    for subView in view.subviews {
+        if let imageView = findHairlineImageViewUnderView(view: subView) {
+            return imageView
         }
-
-        let subviews = (view.subviews)
-        for subview: UIView in subviews {
-            if let imageView: UIImageView = hairlineImageViewInNavigationBar(subview) {
-                return imageView
-            }
-        }
-        return nil
     }
+    return nil
 }
 
 extension UINavigationController {
-
+    
     open override var preferredStatusBarStyle : UIStatusBarStyle {
         guard let viewController = visibleViewController else { return .default }
         return viewController.preferredStatusBarStyle
     }
-
+    
     open override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
         guard let viewController = visibleViewController else { return .portrait }
         return viewController.supportedInterfaceOrientations
     }
-
+    
     open override var shouldAutorotate : Bool {
         guard let viewController = visibleViewController else { return false }
         return viewController.shouldAutorotate
