03ca92fff8d2128a034ae8548eeba27a3eb2a170
Ruvim Micsanschi <ruvim.micsanschi@zimmerbiomet.com>
2019-12-16 14:06:27 -0800

    Package.swift update

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", .exact("2.1.1")),
         .package(url: "https://github.com/cxa/MenuItemKit.git", .exact("3.1.3")),
-        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6")),
+        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6.0")),
         .package(url: "https://github.com/tadija/AEXML.git", .exact("4.4.0")),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("4.1.0")),
         .package(url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.3"))
