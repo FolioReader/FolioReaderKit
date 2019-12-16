0112100082a3257cbc02f54880fdccdb79a43f51
Ruvim Micsanschi <ruvim.micsanschi@zimmerbiomet.com>
2019-12-16 13:55:53 -0800

    Package.swift update

--- Package.swift
+++ Package.swift
@@ -1,14 +1,24 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-	name: "FolioReaderKit",
-	dependencies: [
-		.Package(url: "https://github.com/ZipArchive/ZipArchive.git", majorVersion: 2, minor: 1),
-		.Package(url: "https://github.com/cxa/MenuItemKit.git", majorVersion: 3, minor: 0),
-		.Package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", majorVersion: 0, minor: 6),
-		.Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 2),
-		.Package(url: "https://github.com/ArtSabintsev/FontBlaster.git", majorVersion: 4, minor: 0),
-		.Package(url: "https://github.com/fantim/JSQWebViewController.git", majorVersion: 6, minor: 1),
-		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 1),
-	]
+    name: "FolioReaderKit",
+    
+    products: [
+        .library(name: "FolioReaderKit", targets: ["FolioReaderKit"]),
+    ],
+    
+    dependencies: [
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", .exact("2.1.1")),
+        .package(url: "https://github.com/cxa/MenuItemKit.git", .exact("3.1.3")),
+        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6")),
+        .package(url: "https://github.com/tadija/AEXML.git", .exact("4.4.0")),
+        .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("4.1.0")),
+        .package(url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.3"))
+    ],
+    
+    targets: [
+        .target(name: "FolioReaderKit", dependencies: []),
+        .testTarget(name: "FolioReaderKitTests", dependencies: ["FolioReaderKit"]),
+    ]
 )
